f85c2e8e58688f6faedaae1ee3de35373aae6d5c
Abdulazeez Hassan <aziz.hassan@outlook.com>
2025-01-17 22:16:59 +0000

    fix resource group name

--- Storage-Account/storageaccount.tf
+++ Storage-Account/storageaccount.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = var.resource_group_name_prefix + var.resource_group_name
+  name     = var.resource_group_name_prefix + "-" + var.resource_group_name
 
   tags = {
     environment = var.environment
--- Storage-Account/variables.tf
+++ Storage-Account/variables.tf
@@ -13,7 +13,7 @@ variable "resource_group_name" {
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "rg_"
+  default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
